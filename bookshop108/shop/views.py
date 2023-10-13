a245721f73581cbaeb37077f36703ce0befb551b
assaw <assawin.in@rmuti.ac.th>
2023-10-13 20:37:40 +0700

    before test

--- bookshop108/shop/views.py
+++ bookshop108/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Book
 # Create your views here.
+
+def index(request):
+    books = Book.object.all()
+    return render(request, 'index.html', {'books': books})
